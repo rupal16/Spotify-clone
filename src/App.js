9d4495b6d7912fee4decad6b6ebae4752607664e
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-14 21:46:28 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import Login from "./Login";
 function App() {
   return (
     <div className="app">
-      <h1>Hi there! this is the spotify clone !!</h1>
-
-      {/* spotify logo */}
-      {/* login with spotify button */}
       <Login />
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -5,12 +5,11 @@ import "./Login.css";
 function Login() {
   return (
     <div className="login">
-      {/* spotify logo */}
       <img
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      {/* login with spotify button */}
+      <a>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
