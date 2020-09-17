c604b16a294124a63de58259ddbc1bc0d882682f
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-17 20:36:04 +0530

    basic setup for sidebar, body and footer

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,10 @@
 import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
@@ -10,6 +14,10 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <hr />
     </div>
   );
 }
