f58f73cf91e7a0185de697e59f1c827a1255b150
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-18 11:16:05 +0530

    header component completed

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import SearchIcon from "@material-ui/icons/Search";
 
 import "./Header.css";
 import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "./DataLayer";
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+
   return (
     <div className="header">
       <div className="header__left">
@@ -12,8 +15,8 @@ function Header() {
         <input placeholder="Search for Artists, Songs or" type="text" />
       </div>
       <div className="header__right">
-        <Avatar src="" alt="RJ" />
-        <h4>Rupal Jain</h4>
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
