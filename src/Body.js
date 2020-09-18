2cd4f28839532a9aca040e201787f47a31bd604b
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-18 11:44:50 +0530

    completed 'discover weekly' banner inside body component

--- src/Body.js
+++ src/Body.js
@@ -8,6 +8,18 @@ function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img
+          src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_250/https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly-250x250.png"
+          alt=""
+        />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description...</p>
+        </div>
+      </div>
     </div>
   );
 }
